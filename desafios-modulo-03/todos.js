290e55e575ac9dcbdd99d2398f579cd1e97410ec
paulohdelia <paulohdelia@gmail.com>
2020-08-21 02:18:58 -0300

    add modulo-03 - salvar no local storage

--- desafios-modulo-03/todos.js
+++ desafios-modulo-03/todos.js
@@ -2,12 +2,7 @@ const listElement = document.querySelector('#app ul');
 const inputElement = document.querySelector('#app input');
 const buttonElement = document.querySelector('#app button');
 
-let todos = [
-  'Fazer módulo de JavaScript', 'Fazer módulo de ES6',
-  'Fazer módulo de Node', 'Fazer módulo de React',
-  'Fazer módulo de React Native', 'Fazer GoStack',
-  'Trabalhar na Google',
-]
+let todos = JSON.parse(localStorage.getItem('list_todos')) || [];
 
 function renderTodos() {
   listElement.innerHTML = '';
@@ -41,11 +36,17 @@ function addTodo() {
   inputElement.value = '';
 
   renderTodos();
+  saveToStorage();
 }
 
 function deleteTodo(position) {
   todos.splice(position, 1);
   renderTodos();
+  saveToStorage();
+}
+
+function saveToStorage() {
+  localStorage.setItem('list_todos', JSON.stringify(todos));
 }
 
 buttonElement.onclick = addTodo;
