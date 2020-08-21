7dbf55999f69cb73655c74bc036dc518b2def6fb
paulohdelia <paulohdelia@gmail.com>
2020-08-21 01:49:32 -0300

    add modulo-03 - estruturar app

---
+++ desafios-modulo-03/todos.js
@@ -0,0 +1,10 @@
+const listElement = document.querySelector('#app ul');
+const inputElement = document.querySelector('#app input');
+const buttonElement = document.querySelector('#app button');
+
+let todos = [
+  'Fazer módulo de JavaScript', 'Fazer módulo de ES6',
+  'Fazer módulo de Node', 'Fazer módulo de React',
+  'Fazer módulo de React Native', 'Fazer GoStack',
+  'Trabalhar na Google',
+]
